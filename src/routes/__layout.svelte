3686212741695121164eacb7141a6fc018c559a5
christocarr <christocarr@outlook.com>
2022-06-03 11:19:42 +0100

    feat: add website carbon badge

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,14 @@
 <script context="module">
-	export const load = ({ url }) => {
+	export const load = async ({ url, fetch }) => {
 		const currentRoute = url.pathname;
-
+		//get website carbon badge data
+		const response = await fetch(
+			'https://api.websitecarbon.com/b?url=https://www.iamchriscarr.com'
+		);
 		return {
 			props: {
-				currentRoute
+				currentRoute,
+				data: await response.json()
 			}
 		};
 	};
@@ -18,8 +22,9 @@
 	import Github from '$lib/assets/github.svg';
 	import Instagram from '$lib/assets/instagram.svg';
 	import '../global.css';
-
 	export let currentRoute;
+	export let data;
+	const { c, p, url } = data;
 </script>
 
 <nav>
@@ -44,13 +49,20 @@
 {/key}
 
 <footer>
-	<div class="footer-content">
+	<div class="social-media">
 		<a href="mailto:christocarr@outlook.com"><img src={Email} alt="email icon" /></a>
 		<a href="https://twitter.com/ChrisofCarr"><img src={Twitter} alt="twitter icon" /></a>
 		<a href="https://github.com/christocarr"><img src={Github} alt="github icon" /></a>
 		<a href="https://www.instagram.com/christocarrgrapher/"
 			><img src={Instagram} alt="instagram icon" /></a
 		>
+	</div>
+	<div class="carbon-badge">
+		<div class="inner">
+			<p>{c}g of CO<sub>2</sub>/view</p>
+			<a href="https://www.websitecarbon.com/">Website Carbon</a>
+		</div>
+		<p>Cleaner than {p}% of pages tested</p>
 	</div>
 </footer>
 
@@ -89,24 +101,67 @@
 
 	footer {
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		width: 100vw;
-		height: 90px;
+		padding: 20px;
 		border-top: rgb(223, 223, 223) 1px solid;
 	}
 
-	.footer-content {
+	.social-media {
 		display: flex;
 		justify-content: space-evenly;
 		align-items: center;
 		width: 100%;
 		height: 100%;
+		margin-bottom: 20px;
 	}
 
-	.footer-content a {
+	.social-media a {
 		width: 30px;
 		height: auto;
+	}
+
+	.carbon-badge {
+		font-size: 12px;
+		width: 260px;
+		padding: 10px;
+		text-align: center;
+		background-color: white;
+	}
+
+	.carbon-badge p {
+		font-weight: bold;
+		color: #0e11a8;
+		padding-top: 5px;
+	}
+
+	.carbon-badge .inner {
+		display: flex;
+		width: 100%;
+	}
+
+	.carbon-badge .inner p,
+	.carbon-badge .inner a {
+		width: 50%;
+		text-align: center;
+		padding: 5px 0;
+	}
+
+	.carbon-badge .inner p {
+		height: 100%;
+		margin: 0;
+		color: #0e11a8;
+		border: 1px solid #00ffbc;
+		border-right: none;
+		border-radius: 5px 0 0 5px;
+	}
+
+	.carbon-badge .inner a {
+		color: white;
+		background-color: #0e11a8;
+		border-radius: 0 5px 5px 0;
 	}
 
 	@media screen and (min-width: 768px) {
